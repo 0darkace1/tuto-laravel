f833e0aab9718a6615f053eb17a728e1a78b6b30
0darkace1 <0darkace1@gmail.com>
2025-01-26 01:23:12 +0100

    Edit Post Form

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,22 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\CreatePostRequest;
 use App\Models\Post;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Str;
+use App\Http\Requests\FormPostRequest;
 
 class BlogController extends Controller
 {
     public function create(): View
     {
+        $post = new Post();
+
         // Retourner la vue de création d'article
-        return view("blog.create");
+        return view("blog.create", [
+            "post" => $post
+        ]);
     }
 
-    public function store(CreatePostRequest $request)
+    public function store(FormPostRequest $request)
     {
         $post = Post::create($request->validated());
 
@@ -25,6 +27,23 @@ class BlogController extends Controller
             "slug" => $post->slug,
             "post" => $post->id
         ])->with("success", "Article créé avec succès !");
+    }
+
+    public function edit(Post $post): View
+    {
+        return view("blog.edit", [
+            "post" => $post,
+        ]);
+    }
+
+    public function update(Post $post, FormPostRequest $request)
+    {
+        $post->update($request->validated());
+
+        return redirect()->route("blog.show", [
+            "slug" => $post->slug,
+            "post" => $post->id
+        ])->with("success", "Article modifié avec succès !");
     }
 
     public function index(): View
--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -3,36 +3,6 @@
 @section("title", "Créer un article")
 
 @section("content")
-
-    <div class="card" style="width: 36rem;">
-        <div class="card-body">
-            <form action="" method="POST">
-                @csrf
-
-                <div class="mb-3">
-                    <label for="title" class="form-label">Titre de l'article</label>
-                    <input id="title" name="title" type="text" class="form-control @error('title') is-invalid @enderror" placeholder="Titre de l'article" value="{{old("title")}}" required>
-                    @error('title')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="mb-3">
-                    <label for="slug" class="form-label">Slug de l'article</label>
-                    <input id="slug" name="slug" type="text" class="form-control @error('slug') is-invalid @enderror" placeholder="Slug de l'article" value="{{old("slug")}}">
-                    @error('slug')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="mb-3">
-                    <label for="content" class="form-label">Contenu de l'article</label>
-                    <textarea id="content" name="content" class="form-control @error('title') is-invalid @enderror" placeholder="Contenu de l'article" required>{{old("content")}}</textarea>
-                    @error('content')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                </div>
-
-                <button type="submit" class="btn btn-primary">Enregistrer</button>
-            </form>
-        </div>
-    </div>
+    @include("blog.form")
 @endsection
+
